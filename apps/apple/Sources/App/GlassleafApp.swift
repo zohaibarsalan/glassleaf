ea5e199ea2eab0e26bac413871def047179333f6
Zohaib Arsalan <zohaibhstart@gmail.com>
2026-07-18 19:34:27 +0500

    set a comfortable minimum Mac window size

--- apps/apple/Sources/App/GlassleafApp.swift
+++ apps/apple/Sources/App/GlassleafApp.swift
@@ -6,7 +6,10 @@ struct GlassleafApp: App {
 #if os(macOS)
         WindowGroup {
             AppRootView()
+                .frame(minWidth: 860, minHeight: 640)
         }
+        .defaultSize(width: 1180, height: 820)
+        .windowResizability(.contentMinSize)
         .windowStyle(.hiddenTitleBar)
         .windowToolbarStyle(.unifiedCompact(showsTitle: false))
 #else
